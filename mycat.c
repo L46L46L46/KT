cb7ca84f22ab0efd20bd1f1a6dea6022016d2950
L46L46L46 <55983179+L46L46L46@users.noreply.github.com>
2020-09-25 02:59:57 +0300

    Update mycat.c

--- mycat.c
+++ mycat.c
@@ -6,36 +6,27 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-int from_input_to_stdout(int from, int to, int file_or_stdin)
+int from_input_to_stdout(int from, int to)
 {
 	size_t buf_size = 4096;
-	char* buf = malloc(buf_size);
-	int rread = read(from, buf, buf_size);
-	if (rread < 0)
-	{
-		perror("Error: ");
-		free(buf);
-		close(from);
-		return errno;
-	}
-	while(rread > 0 && file_or_stdin == 0)
+	char* buf = malloc(buf_size);	
+	int rread;
+	while((rread = read(from, buf, buf_size)) > 0)
 	{
 		int wwrite = write(to, buf, rread);
 		if (wwrite != rread)
 		{
-			perror("Error: ");
 			free(buf);
 			close(from);
+			printf("Error: ");
 			return errno;
 		}
-		rread = read(from, buf, buf_size);
-		file_or_stdin = from;
 	}
 	free(buf);
-	close(from);
 	if (rread < 0)
-	{
-		perror("Error: ");
+	{		
+		close(from);
+		printf("Error: ");
 		return errno;
 	}
 	return 0;
@@ -46,9 +37,9 @@ int main(int argc, char** argv)
 	if (argc == 1)
 	{
 
-		return from_input_to_stdout(0, 1, 0);
+		return from_input_to_stdout(0, 1);
 	}
-	for (int i = 1; i <= argc; i++)
+	for (int i = 1; i < argc; i++)
 	{
 		int from = open(argv[i], O_RDONLY, 0777);
 		if (from < 1)
@@ -56,10 +47,11 @@ int main(int argc, char** argv)
 			perror("Error: ");
 			return errno;
 		}
-		int ret = from_input_to_stdout(from, 1, 0);
+		int ret = from_input_to_stdout(from, 1);
 		if (ret != 0)
 		{
 			return ret;
 		}
+		close(from);
 	}
 }
